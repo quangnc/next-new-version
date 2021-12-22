b89125f2490f4ef0a9db897e96f3c0ee0cbb0b9f
Nguyen Chi Quang <chiquang.dev@gmail.com>
2021-12-22 15:33:31 +0700

    install tailwindcss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+
+import "@styles/tailwind.css";
+import "@styles/shared.css";
+import "@styles/nprogress.css";
+import "@styles/custom.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
